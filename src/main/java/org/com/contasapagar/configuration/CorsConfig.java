84b8b635aa6b337caacab04e80b9748c78ee6f63
João Batista <j.batista.damasceno@icloud.com>
2024-07-25 01:54:41 -0300

    main- ajustes para terraform

--- src/main/java/org/com/contasapagar/configuration/CorsConfig.java
+++ src/main/java/org/com/contasapagar/configuration/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://ec2-54-82-198-219.compute-1.amazonaws.com:3000")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
